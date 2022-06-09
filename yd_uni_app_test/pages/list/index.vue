43966daac392eafdc86b07835d310ffb648ba00e
yangda <1341521959@qq.com>
2022-06-09 14:29:10 +0800

    test for pdf

---
+++ yd_uni_app_test/pages/list/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="container">
+		
+		<view class="intro">yd 即可查看组件文档。</view>
+		<text class="intro">详见：</text>
+		<uni-link :href="href" :text="href"></uni-link>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://www.baidu.com'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
